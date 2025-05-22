c39a38b161addb7eb19258cc386562d2ef5b3535
ValentinOneto <49261732@est.ort.edu.ar>
2025-05-22 10:45:07 -0300

    colisiones

---
+++ Assets/Scripts/deteccionDeColisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
